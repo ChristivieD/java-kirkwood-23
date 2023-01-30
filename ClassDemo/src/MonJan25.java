685fe7c2ad63ed1bee337100b862044a36d944a7
Christivie Mauwa <lisedjuma@gmail.com>
2023-01-29 19:54:04 -0600

    Created my first Java class

---
+++ ClassDemo/src/MonJan25.java
@@ -0,0 +1,45 @@
+public class MonJan25 {
+    public static void main (String[] args){
+        //java comment
+        System.out.println("Hello") ;
+
+
+
+
+
+
+
+
+
+        String myFirstName = "Christivie";
+        String myLAstName = "Mauwa";
+        char middleInitial = 'D';
+        int myAge = 20;
+        Character myMiddleInitial = 'D';
+        Integer age = 20;
+        double myWeight = 190.5;
+        double weight = 190.5;
+        boolean likesPizza = true;
+        float currentTemp =  30;
+        long reallyLongNumber = 9999999999999999L;
+        int reallyLongInt = 99999999;
+        int biggestInt = Integer.MAX_VALUE;
+        int a = 7;
+        int b =2;
+        System.out.println(a / b); // Python would print 3.5, Java print 3
+        System.out.print(a + b); // 9
+        System.out.println(a - b); // 5
+        System.out.println(a * b); // 14
+        System.out.println(a % b); //1
+        int c= 3;
+        System.out.println (a + b* c); // 13
+        System.out.println((a +b) * c); //27
+        System.out.println(Math.min(a, b)); // 2")
+
+
+
+
+
+
+    }
+}
