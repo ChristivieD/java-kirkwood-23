2e43fff9e0334f3c98200840f27bf3f0179bfcc3
Christivie Mauwa <lisedjuma@gmail.com>
2023-02-27 11:49:42 -0600

    Finished Assignment2

--- PartnerAssignments/src/Assignment2.java
+++ PartnerAssignments/src/Assignment2.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class Assignment2 {
     public static void main(String[] args) {
-        // navigator:
-        // driver:
+        // navigator: Christivie
+        // driver: Tyler
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        double number = sc.nextDouble();
+        if(number % 2 == 0){
+            System.out.println("Even");
+        } else if(number % 2 == 1){
+            System.out.println("Odd");
+        } else{
+            System.out.println("Neither");
+        }
+        //navigator:Tyler
+        // driver: Christivie
+        System.out.println("Enter seconds: ");
+        int seconds = sc.nextInt();
+        int days = seconds / 86400;
+        int daysSeconds = seconds % 86400;
 
-        //navigator:
-        // driver:
+        int hours = daysSeconds / 3600;
+        int hoursSeconds = daysSeconds % 3600;
+
+        int mins = hoursSeconds / 60;
+        int minSecs = daysSeconds % 60;
+
+        System.out.printf("Days: %d\nHours: %d\nMinutes: %d\nSeconds: %d,", days, hours, mins,minSecs);
     }
 }
